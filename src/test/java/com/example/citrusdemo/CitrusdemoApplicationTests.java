3640525694d1d3a4b4b016660a18ab6fbaf87c00
Aniket kumar <aniketkumar@celerispay.com>
2024-02-21 14:00:27 +0530

    removed failed test

--- src/test/java/com/example/citrusdemo/CitrusdemoApplicationTests.java
+++ src/test/java/com/example/citrusdemo/CitrusdemoApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.citrusdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CitrusdemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
